33b445bc250ce6d330dbb4c45b8988299ff97b1b
Anna-Jaison <anaajai0704@gmail.com>
2025-02-01 20:53:22 +0530

    ädded backgroundImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import LandingPage from "./pages/LandingPage/LandingPage";
+import pattern from "./assets/Pattern.png"
 function App() {
   return (
-    <>
+    <><div className="w-screen h-screen" style={{backgroundImage:`url(${pattern})`}}>
       <LandingPage />
+    </div>
+     
+      
     </>
   );
 }
