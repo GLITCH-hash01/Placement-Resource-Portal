d1cf82d43f6a1d2c1d73dedb1c8c384218cbc5b4
Glitch-hash01 <harikichus2018@gmail.com>
2025-02-01 22:16:18 +0530

    Working on Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
+import Logo from "../assets/PRPLogo.png";
+
 export default function Navbar() {
   return (
     <>
-      <nav></nav>
+      <nav className="w-screen h-20 bg-white flex ">
+        <img src={Logo} alt="" className="w-10 h-10" />
+        <div className="bg-white w-140 h-18 border-2 border-black rounded-xl flex  gap-5 font-primary ">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Services</a>
+          <a href="">Contact Us</a>
+        </div>
+        
+      </nav>
     </>
   );
 }
