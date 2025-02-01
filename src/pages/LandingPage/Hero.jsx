cdd4aaad71da692ffe2711022a283d6b869b4f8e
Anna-Jaison <anaajai0704@gmail.com>
2025-02-01 22:05:36 +0530

    added main text and main vector

--- src/pages/LandingPage/Hero.jsx
+++ src/pages/LandingPage/Hero.jsx
@@ -1,7 +1,15 @@
+import vector from "../../assets/vector main.png";
 export default function Hero() {
   return (
     <>
-      <div></div>
+      <div className=" absolute bottom-85 left-22 text-black  ">
+    <h1 className="text-7xl leading-24 font-primary font-bold text-left "> Placement Resource <br/>Portal</h1>
+      </div>
+      <div className="absolute bottom-27
+     right-23">
+        <img src={vector} className="size-130"></img>
+
+      </div>
     </>
   );
 }
