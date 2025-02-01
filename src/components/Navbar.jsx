46819905c3668913c80f1d975bb740cf7ae9a1eb
Anna-Jaison <anaajai0704@gmail.com>
2025-02-02 01:23:33 +0530

    updated hero and nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ export default function Navbar() {
   return (
     <>
       <nav className="w-screen h-fit flex justify-around items-center gap-10  p-4">
-        <img src={Logo} alt="" className="w-12 h-12" />
-        <div className="w-120 h-fit py-3 bg-white border-2 font-semibold border-black rounded-xl flex gap-5 items-center justify-center">
+        <img src={Logo} alt="" className="w-15 h-15" />
+        <div className="w-190 h-fit py-3 bg-white border-2 font-bold text-2xl border-black rounded-xl flex gap-5 items-center justify-center">
           <a href="" className="px-3 bg-primary">
             Home
           </a>
@@ -13,7 +13,7 @@ export default function Navbar() {
           <a href="" className="px-3">Services</a>
           <a href="" className="px-3">Contact Us</a>
         </div>
-        <button className="bg-primary px-8 border-2 rounded-xl  border-black py-2 cursor-pointer " style={{boxShadow:"0px 4px 0px 0px #000"}}>Login</button>
+        <button className="bg-primary px-8 border-2 rounded-xl text-2xl font-bold border-black py-2 cursor-pointer " style={{boxShadow:"0px 4px 0px 0px #000"}}>Login</button>
       </nav>
     </>
   );
