b5bd0c6ae1bb11c2c525397c4872d0e7861f6ae4
Glitch-hash01 <harikichus2018@gmail.com>
2025-02-01 23:46:45 +0530

    Added Signin,Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import LandingPage from "./pages/LandingPage/LandingPage";
+import Signin from "./pages/Authorization/Signin.jsx";
 import pattern from "./assets/Pattern.png";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
       <div
         className="w-screen h-screen"
         style={{ backgroundImage: `url(${pattern})` }}>
-        <LandingPage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Signin />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
