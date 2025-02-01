565544fc755328fc9e87827e4d8d7ddfeaaafba3
Anna-Jaison <anaajai0704@gmail.com>
2025-02-01 23:32:26 +0530

    changed hero

--- src/pages/LandingPage/Hero.jsx
+++ src/pages/LandingPage/Hero.jsx
@@ -2,14 +2,18 @@ import vector from "../../assets/vector main.png";
 export default function Hero() {
   return (
     <>
-      <div className=" absolute bottom-85 left-22 text-black  ">
-    <h1 className="text-7xl leading-24 font-primary font-bold text-left "> Placement Resource <br/>Portal</h1>
+      <div className="flex justify-center items-center gap-50 ">
+      
+      <div className="flex-col">
+      <h1 className="text-7xl leading-24 font-primary font-bold text-left "> Placement Resource <br/>Portal</h1>
+      <p></p>
       </div>
-      <div className="absolute bottom-27
-     right-23">
-        <img src={vector} className="size-130"></img>
-
+      <img src={vector} className="size-130"></img>
+      
       </div>
+     
+      
+     
     </>
   );
 }
