3f68193455af3f374c9b5c027b922fb1c53c2a47
Glitch-hash01 <harikichus2018@gmail.com>
2025-02-02 21:43:54 +0530

    Added Sidebar file,FavIcon,Title

--- src/pages/Authorization/Signin.jsx
+++ src/pages/Authorization/Signin.jsx
@@ -8,7 +8,7 @@ export default function Signin() {
       <div className="w-screen h-fit flex fixed justify-around items-center gap-200 p-4">
         <img src={logo} alt="" className="w-12 h-12" />
         <button
-          className="bg-primary px-8 border-2 rounded-xl  border-black py-2 cursor-pointer font-primary font-bold"
+          className="bg-primary px-8 border-2 rounded-xl border-black py-2 cursor-pointer font-primary font-bold"
           style={{ boxShadow: "0px 4px 0px 0px #000" }}>
           Sign up
         </button>
