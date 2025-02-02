b0bb2195c82237bb559d68d85b02a1f7742a913d
Glitch-hash01 <harikichus2018@gmail.com>
2025-02-02 11:10:59 +0530

    Added Services

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/Navbar";
 import Hero from "./Hero";
+import Services from "./Services";  
 export default function LandingPage() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Services />
     </>
   );
 }
