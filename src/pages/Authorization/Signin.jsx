1adf22241c4573fd1a4319c2e044ec12b7835d76
Shijina843 <shijinashajeem37@gmail.com>
2025-02-02 17:01:56 +0530

    sign in

--- src/pages/Authorization/Signin.jsx
+++ src/pages/Authorization/Signin.jsx
@@ -1,3 +1,47 @@
+import logo from "../../assets/PRPLogo.png";
+import rightimage from "../../assets/vector right.png";
+import leftimage from "../../assets/vector left.png";
 export default function Signin() {
-  return <></>;
+  return (
+  <>
+  <div className="w-screen h-fit flex justify-around items-center gap-200 p-4">
+    <img src={logo} alt="" className= "w-12 h-12"/>
+    <button className="bg-primary px-8 border-2 rounded-xl  border-black py-2 cursor-pointer font-primary font-bold" style={{boxShadow:"0px 4px 0px 0px #000"}}>Sign Up</button>
+  </div>
+
+  <div className="flex justify-center items-center px-8 relative font-primary gap-8">
+  <div className="flex justify-center items-center ">
+          <img src={leftimage} alt=" " className="w-[200px] h-auto" />
+        </div>
+    <div className="w-100 h-130 py-16  bg-black rounded-xl " style={{boxShadow:"5px 5px 0px 0px #9B9BFF"}}>
+       <h2 className="text-3xl text-primary font-bold text-center mb-4 ">Login</h2>
+        <p className="text-sm text-white p-10">Hey, Enter your details to Sign into your account</p>
+        <form className=" flex flex-col gap-4 px-10">
+            <input type="email" id="email" className="w-80 h-10 p-4 rounded-3xl bg-white text-black text-sm"
+                placeholder="Email Address" style={{boxShadow:"3px 3px 0px 0px #9B9BFF"}}/>
+                <input
+                type="password"
+                id="password"
+                className="w-80 h-10 p-4 rounded-3xl bg-white text-black text-sm"
+                placeholder="Password" style={{boxShadow:"3px 3px 0px 0px #9B9BFF"}}/>
+                <p className="text-sm text-left text-white">Having trouble in sign-in?</p>
+                <button
+              type="submit"
+              className="bg-primary hover:bg-blue-600 px-4 border-2 border-black py-2 rounded-xl font-bold" style={{boxShadow:"0px 2px 0px 0px #9B9BFF "}}>Sign in
+            </button>
+            <p className="text-sm text-center mt-4 text-white">
+            Don't have an Account?{" "}
+            <a href="" className="text-blue-400 hover:underline">
+              Register Now
+            </a>
+          </p>
+        </form>
+    </div>
+    <div className="flex justify-around items-center">
+          <img src={rightimage} alt=" " className="w-[400px] h-[400px] px-10" />
+        </div>
+  </div>
+
+  </>
+  );
 }
