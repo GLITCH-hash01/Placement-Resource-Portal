41a2670b6423ead966867934588945c7429193ea
Glitch-hash01 <harikichus2018@gmail.com>
2025-02-20 01:33:14 +0530

    Fixed:Routing,Added:Conditional role based login

---
+++ src/pages/Alumni/Roadmaps/Roadmap.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Heading from '../../../components/Heading'
+export default function Roadmap() {
+  return (
+    <Heading>Roadmap</Heading>
+  )
+}
---
+++ src/pages/Faculty/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+
+import Sidebar from "../../components/Sidebar";
+import Search from "../../components/Search";
+import ProfileBar from "../../components/ProfileBar";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="w-screen h-screen p-8 flex justify-center items-center gap-10">
+        <Sidebar active={"Dashboard"} />
+        <div className="w-full h-full flex flex-col  gap-8">
+          <div className="flex gap-10 justify-between px-10 items-center">
+            <Search />
+            <ProfileBar />
+          </div>
+          <div className="w-full h-full">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
