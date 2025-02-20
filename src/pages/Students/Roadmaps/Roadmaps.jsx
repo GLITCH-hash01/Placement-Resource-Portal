fdbe209170c15be6479d3f316e9b883d8c92f1b3
Glitch-hash01 <harikichus2018@gmail.com>
2025-02-20 23:45:30 +0530

    Fixed Arrow

--- src/pages/Students/Roadmaps/Roadmaps.jsx
+++ src/pages/Students/Roadmaps/Roadmaps.jsx
@@ -20,50 +20,70 @@ export default function Roadmaps() {
         <div className="absolute top-[50%] gap-20 h-72 w-full flex items-center  -translate-y-[50%]">
           j{" "}
           <div className="h-full flex flex-col items-center justify-center gap-10 ">
-            <div className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2" style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
-            <p>Year 1</p>
+            <div
+              className="bg-primary cursor-pointer text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2"
+              style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
+              <p>Year 1</p>
             </div>
             <div className=" bg-white h-25   w-25 rounded-full flex flex-center">
-            <IoCompassOutline size={100}/>
+              <IoCompassOutline size={100} />
             </div>
-            <div className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center" style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
-            <p>Foundation and Basics </p><span className="-rotate-100 text-xl">
-                <IoArrowForwardCircle />
-              </span>
+            <div
+              className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center"
+              style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
+              <p className="flex items-center justify-center gap-2">
+                Foundation and Basics{" "}
+                <span className=" text-xl bg-black  -rotate-45 text-white rounded-full">
+                  <IoArrowForwardCircle  />
+                </span>{" "}
+              </p>
             </div>
           </div>
           <div className="h-full flex flex-col items-center justify-center gap-10">
-            <div className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2" style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
-            <p>Year 2</p>
+            <div
+              className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2"
+              style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
+              <p>Year 2</p>
             </div>
-            <div className=" bg-white h-25   w-25 rounded-full flex items-center justify-center" style={{ marginTop: "8px" }}>
-            <FaCalendarCheck size={68}/>
+            <div
+              className=" bg-white h-25   w-25 rounded-full flex items-center justify-center"
+              style={{ marginTop: "8px" }}>
+              <FaCalendarCheck size={68} />
             </div>
-            <div className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center" style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
-            <p>Intermediate Level & Core Subject</p>
+            <div
+              className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center"
+              style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
+              <p>Intermediate Level & Core Subject</p>
             </div>
           </div>
           <div className="h-full flex flex-col items-center justify-center gap-10 ">
-            <div className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2" style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
-            <p>Year 3</p>
+            <div
+              className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2"
+              style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
+              <p>Year 3</p>
             </div>
             <div className=" bg-white h-25   w-25 rounded-full">
-            <PiStudentBold size={90}/>
+              <PiStudentBold size={90} />
             </div>
-            <div className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center" style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
-            <p>Specialization & Industry Readiness
-            </p>
+            <div
+              className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center"
+              style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
+              <p>Specialization & Industry Readiness</p>
             </div>
           </div>
-          <div className="h-full flex flex-col items-center justify-center gap-10 ml-18">
-            <div className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2" style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
-            <p>Year 4</p>
+          <div className="h-full flex flex-col items-center justify-center gap-10 ">
+            <div
+              className="bg-primary text-black font-primary px-3 py-2 rounded-xl font-bold text-center border-black border-2"
+              style={{ boxShadow: "0px 4px 0px 0px #000000" }}>
+              <p>Year 4</p>
             </div>
-            <div className=" bg-white h-25   w-25 rounded-full flex items-center justify-center" style={{ marginTop: "15px" }}>
-            <MdOutlineWorkHistory size={75}/>
+            <div className=" bg-white h-25   w-25 rounded-full flex items-center justify-center">
+              <MdOutlineWorkHistory size={75} />
             </div>
-            <div className="bg-primary text-black font-primary px-3 py-3 rounded-xl font-bold text-center" style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
-            <p>Placement & Career Preparation</p>
+            <div
+              className="bg-primary text-black font-primary px-3 py-3 rounded-xl font-bold text-center"
+              style={{ boxShadow: "0px 4px 5px 0px #000000" }}>
+              <p>Placement & Career Preparation</p>
             </div>
           </div>
         </div>
