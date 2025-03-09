27330c739f254cf50843f42f173726dcd481aaf7
Glitch-hash01 <harikichus2018@gmail.com>
2025-03-09 21:30:47 +0530

    Added Heading to Notes

--- src/pages/Students/Notes.jsx
+++ src/pages/Students/Notes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "../../components/Card";
 import star from "../../assets/images/star.png";
+import Heading from "../../components/Heading";
 
 export default function Notes() {
   return (
     <>
       <div className="flex flex-col w-full h-full p-8 overflow-y-scroll">
+        <Heading>Academic Notes</Heading>
         <div className="grid grid-cols-4 gap-3 mb-8 bg-four p-12 rounded-3xl">
           <Card title={"CST 201"} desc={"Algorithm Analysis and Design"} />
           <Card title={"CST 202"} desc={"Data Structures"} />
